e26d8256c8b62c6d9a0f9ba280f1afa64fced5a3
Tcaprioli <Capriolityler@gmail.com>
2020-03-15 16:43:12 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,13 +5,77 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
-class App extends Component {
 
+
+class App extends Component {
+  state={
+    sushis: [],
+    sushiIndex: {
+      start:0,
+      end:4
+    },
+    budget:100,
+    sushiEaten:[]
+  }
+
+  async componentDidMount(){
+    let resp = await fetch(API)
+    let sushiArray = await resp.json()
+    this.setState({sushis: sushiArray})
+  }
+
+  nextFour =()=> {
+  (this.state.sushiIndex.end < 100 ?
+   this.setState({
+     sushiIndex: {
+       start: this.state.sushiIndex.start += 4,
+       end: this.state.sushiIndex.end += 4}
+    })
+    :
+    this.setState({
+      sushiIndex: {
+        start: this.state.sushiIndex.start = 0,
+        end: this.state.sushiIndex.end = 4}
+     }))
+    console.log(this.state.sushiIndex.end)
+   }
+ 
+   eatSushi=(sushiObj)=>{
+    let {sushis} = this.state
+   
+   let updatedArray = sushis.map(sushi => {
+      if(sushi.id === sushiObj.id){
+        return {
+          ...sushi,
+          eaten:true}}
+      else{return sushi}
+    })//end of map
+    this.setState({sushis: updatedArray})
+    
+   }
+
+   buySushi=sushiObj=>{
+    let {budget} = this.state
+    let dirtyPlate = this.state.sushiEaten
+
+    if (budget >= sushiObj.price && sushiObj.eaten !== true) 
+    {this.setState({budget: budget - sushiObj.price})
+    this.eatSushi(sushiObj)
+    dirtyPlate.push(sushiObj)
+    this.setState({sushiEaten: dirtyPlate})
+  }
+    
+    
+   }
+
+  
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        {}
+        <SushiContainer sushiData={this.state.sushis} sushiIndex={this.state.sushiIndex} 
+        nextFour={this.nextFour} eaten={this.state.eaten} buySushi={this.buySushi} budget={this.state.budget}/>
+        <Table budget={this.state.budget} sushiEaten={this.state.sushiEaten}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -4,17 +4,17 @@ const Sushi = (props) => {
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={event=>props.buySushi(props)}>
         { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
+          props.eaten? 
+          
             null
           :
-            <img src={/* Give me an image source! */ } width="100%" />
+            <img src={props.img_url } width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {props.name} - ${props.price}
       </h4>
     </div>
   )
